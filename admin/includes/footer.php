733285d22ef4c916c088101b15b462288adf7756
baeshiii <144511933+baeshiii@users.noreply.github.com>
2023-12-06 11:01:51 +0800

    Case Study Web

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+    <b>Case Study for Web and Mobile Development</b>
+    </div>
+    <strong>Copyright &copy; 2023 Brought to You By <a style="color: inherit;">Chavez, Cinco, Eblamo, Maleniza, Zambale</a></strong>
+</footer>
---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer" style="background-color: darkgray">
+    <div class="container">
+        <div class="pull-right hidden-xs">
+            <b>Case Study for Web and Mobile Development</b>
+        </div>
+        <strong>Copyright &copy; 2023 Brought to You By <a style="color: inherit;">Chavez, Cinco, Eblamo, Maleniza, Zambale</a></strong>
+    </div>
+</footer>
