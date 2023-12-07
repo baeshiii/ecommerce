96033633794b34862f2782b6254b313b69a39529
Tristan Sean Paul Perfecto Cinco <tristanseanpaul.cinco@gmail.com>
2023-12-07 08:00:55 +0800

    About and Contact Pages Done

---
+++ contact.php
@@ -0,0 +1,60 @@
+<?php include 'includes/session.php'; ?>
+<?php include 'includes/header.php'; ?>
+<body class="hold-transition skin-blue layout-top-nav">
+<div class="wrapper">
+
+	<?php include 'includes/navbar.php'; ?>
+  <link rel="stylesheet" href="dist/css/ContactUs.css">
+  <!-- <link rel="stylesheet" href="dist/css/About.css"> -->
+	 
+	  <div class="content-wrapper">
+	    <div class="container">
+
+	      <!-- Main content -->
+	      <section class="content">
+          <div class="row head-main">
+            <h2>Contact Us</h2>
+          </div>
+	        <div class="row contact">
+	        	<div class="col-sm-12 col-md-8 image">
+              <img class="img-fluid" src="images/contact-map.png" alt="">
+	        	</div>
+	        	<div class="col-sm-12 col-md-4 text">
+              <div class="head">
+                <h2>MerkadoPH</h2>
+              </div>
+              <div class="body">
+                <div class="address info">
+                  <i class="fa fa-map"></i>
+                  <div class="text">
+                    <p>900 San Marcelino St, </p>
+                    <p>Ermita, Manila,</p>
+                    <p>1000 Metro Manila</p>
+                  </div>
+                </div>
+                <div class="email info">
+                  <i class="fa fa-envelope"></i>
+                  <div class="text">
+                    <p>info@adamson.edu.ph</p>
+                  </div>
+                </div>
+                <div class="phone info">
+                  <i class="fa fa-phone"></i>
+                  <div class="text">
+                    <p>(02) 8524 2011</p>
+                  </div>
+                </div>
+              </div>
+	        	</div>
+	        </div>
+	      </section>
+	     
+	    </div>
+	  </div>
+  
+  	<?php include 'includes/footer.php'; ?>
+</div>
+
+<?php include 'includes/scripts.php'; ?>
+</body>
+</html>
--- includes/navbar.php
+++ includes/navbar.php
@@ -12,8 +12,8 @@
       <div class="collapse navbar-collapse pull-left" id="navbar-collapse">
         <ul class="nav navbar-nav">
           <li><a href="index.php">HOME</a></li>
-          <li><a href="">ABOUT US</a></li>
-          <li><a href="">CONTACT US</a></li>
+          <li><a href="about.php">ABOUT US</a></li>
+          <li><a href="contact.php">CONTACT US</a></li>
           <li class="dropdown">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">CATEGORY <span class="caret"></span></a>
             <ul class="dropdown-menu" role="menu">
